0159054ff546815f049c73bf5b6b77a11ccb96f6
wrishav1 <99461741+wrishav1@users.noreply.github.com>
2022-05-15 22:16:23 +0545

    Add files via upload

---
+++ src/components/TnC.js
@@ -0,0 +1,57 @@
+import React, { useRef} from 'react'
+function TnC() {
+    return(
+        <div  className="container">
+            <ul>
+TERMS AND CONDITIONS FOR SALE
+These terms and conditions are applicable only to the sale of fixed-price Objects on the Saffronart site and not to auction works. For terms and conditions related to specific auctions, please check the conditions for sale for that auction. For terms and conditions related to the sale of immoveable property (i.e. real estate), please check the conditions for sale in the Prime Properties section of the website
+
+Definition:
+
+"Buyer" shall mean a person who is registered as a user to access the Website and buys a Object from the Website.
+"Description" means all the details of a Object as set out in the Website and may include, without limitation, the name of the artist, designer or manufacturer, the title of the Object, and the following additional details:
+
+For artworks: the signature of the artist/ date of the Object (if any), the surface, medium and dimensions, year, provenance and any additional information related to the Object.
+
+For jewellery, watches and precious stones: any other features or weight or characteristics of the Object mentioned on the Website
+
+"Object" means the artwork, jewellery, watches, loose precious stones and other such articles offered by Saffronart for sale either individually or collectively (as a single unit) through the Website.
+
+General terms:
+
+The Object made available for sale on the Website may not always be in the physical possession of Saffronart and may be physically located anywhere in the world.
+
+All Objects made available for sale on the Website will be sold at the price listed and no discounts will be granted to the Buyer. The price of the Object listed does not include duties, taxes, shipping and handling, which will be charged as applicable and would depend on the physical location of the Object as well as the shipping address provided by the Buyer. The information provided during registration or amended at the time of finalizing a purchase shall be the final billing address and shipping address for delivery of the Object.
+
+The Buyer agrees that the Buyer is liable for any duties and taxes that might be applicable on the purchase as per clause 2 above and such charges shall be payable in addition to the price of the Object.
+
+All invoices will be raised by Saffronart based on the registration information provided by the Buyer or amended at the time of finalizing a purchase and payments will be accepted only from the Buyer so registered. In the event that a payment is sent from elsewhere, it may either be returned or could result in a delay in the processing of the order until management decides whether a variation from the registration details may be accepted.
+
+Placing an order on www.Mandala.com constitutes an irrevocable acceptance of the purchase. Unless rescinded/cancelled by Saffronart within 2 working days on account of a default by the seller or any prior or continuing breach by the Buyer, such acceptance results in an enforceable contact of sale. The contract of sale is between Saffronart (acting as the agent of the seller) and the Buyer.
+
+Saffronart shall have the right to cancel any order placed on the website www.Mandala.com, at any time prior to the delivery of the Object, without assigning any reason whatsoever.
+
+Saffronart shall assume no responsibility for any errors or ommissions that may occur in the Description, pricing or other content related to the Object. In the event of such an error, Saffronart reserves the right to cancel the order placed by the Buyer by informing the Buyer in writing.
+
+Once an order is placed on Saffronart the Buyer is required to make payment for the price of the Object as well as the charges referred to above within 7 working days from the date of placing the order. In the event that Saffronart does not receive payment within 7 working days Saffronart reserves the right to cancel the order without prior notice to the Buyer. Alternately, and without prejudice to the foregoing, Saffronart may, at its absolute discretion, hold the Object for the Buyer and charge any storage and insurance costs applicable on the purchase and/or may seek to commence legal proceedings (acting as the agent of the Seller) to ensure execution of the contract of sale.
+
+The Object will be delivered/shipped to the Buyer once all necessary documentation, relating to transportation, tax, etc. as may be reasonably requested for by Saffronart are completed and payment has been realized in full including any additional charges. Transit insurance costs will be included in such additional charges, unless specifically advised otherwise by the Buyer. In the event that the Buyer chooses to himself collect the Object from Saffronart he must do so within two weeks of Saffronart receiving the payment. In the event that the Object is not so collected within a period of two weeks, storage and insurance costs will be applicable on the purchase and will be determined at the complete discretion of Saffronart.
+
+You agree to abide by all provisions prescribed in these terms and conditions. In particular, you warrant that all information that you submit will be true and accurate (including without limitation your credit card number and expiration date), and you agree to pay all costs, charges plus all applicable taxes for the purchases made by you.
+
+Saffronart is authorized by the sellers to exercise complete discretion in (i) the illustration and descriptions of Object for viewing and sale and (ii) acceptance or rejection of orders received.
+
+All Objects (other than jewellery, watches and precious stones) displayed on the Website is supported by an authenticity guarantee from the Seller.
+
+Saffronart is not obligated to provide any physical preview of the Object either before or after the sale. Saffronart is in no way liable for the condition of the Object. Condition reports are provided on request and are based on the information provided by the seller. In the event that Saffronart is of the view that there is a significant difference in the physical aspect relative to its appearance as displayed on the website, an email will be sent to the Buyer regarding the difference and Saffronart will await the Buyer's decision to proceed with the order.
+
+All foreign currency exchange rates for the purchase are made on a constant of 1:125(USD:NPR). The prices for individual Object have been 'rounded off' for ease of reference and do not necessarily reflect the exact exchange rate that will be used for calculation of the invoiced price.
+
+The Buyer warrants that all the information furnished at the time of registration is true and correct and any change in the information provided will be updated and intimated to Saffronart immediately. Saffronart is entitled to act on the basis of the information provided and seek such further information, clarifications or verifications as it may deem necessary for granting registration. Such registration may be suspended or cancelled where Saffronart forms a reasonable view that any of the above information may be inaccurate. However, such registration will be restored immediately on the buyer furnishing information to the satisfaction of SaffronArt.
+
+Please refer to the Disclaimer and the terms of our Privacy Policy, which together with these terms and conditions govern your use of the website.
+</ul>
+</div>
+    );
+}
+export default TnC;
